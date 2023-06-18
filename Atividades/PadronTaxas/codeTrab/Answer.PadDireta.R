33b1da76db66beb7074c197f5d7573815464a87f
czargab18 <cesagabrielcode@gmail.com>
2023-06-17 23:34:35 -0300

    [Ã4.2] - PadronDireta

--- Atividades/PadronTaxas/codeTrab/Answer.PadDireta.R
+++ Atividades/PadronTaxas/codeTrab/Answer.PadDireta.R
@@ -8,7 +8,7 @@
 
 # cálculo da TBM e TEM e PADRONIZAÇÃO----
 
-dadoCompleto<-
+tbemDado<-
   dadoCompleto |> 
   mutate(
     # TBM_TEM = map2( .x = numObitos, .y = numPopulacao, .f = ~((.x/.y)*1000) ) # TBM
@@ -17,7 +17,7 @@ dadoCompleto<-
   unnest(TBM_TEM) |> 
   pivot_wider( names_from = regiao, values_from = numObitos:TBM_TEM) |> 
   select(
-    "idadeSimples","TBM_TEM_Brazil","TBM_TEM_United Republic of Tanzania","TBM_TEM_WORLD"
+    "idadeSimples","numPopulacao_WORLD","numObitos_WORLD","TBM_TEM_Brazil","TBM_TEM_United Republic of Tanzania","TBM_TEM_WORLD"
   ) |> 
   mutate(
     idadeSimples =  factor(idadeSimples,
@@ -28,14 +28,9 @@ dadoCompleto<-
                            )
     )
   ) |> 
-  arrange(idadeSimples) |> 
-  rename(
-    "tbemBrazil" = "TBM_TEM_Brazil",
-    "tbemTanzania" = "TBM_TEM_United Republic of Tanzania",
-    "tbemWorld" = "TBM_TEM_WORLD"
-    
-  )
+  arrange(idadeSimples)
 
+tbemDado
 # .final
 
 
@@ -46,61 +41,69 @@ dadoCompleto<-
 
 
 dadoCalculado<-
-  dadoCompleto |> 
+  tbemDado |> 
   mutate(
-    Brasil = map2(.x = tbemBrazil, .y = tbemWorld, .f = ~(.x*.y)),
+    padBrasil = map2( .x = tbemBrazil, .y = tbemWorld, 
+                      .f = ~((.x*.y))), # TEM BRAZIL * POPULAÇÃO MUNDIAL
     
-    Tanzania = map2(.x = tbemTanzania, .y = tbemWorld,.f = ~(.x*.y) ) 
-  ) |>  
-  unnest(c(Brasil,Tanzania)) |> 
-  filter(
-    !idadeSimples %in% "Total"
-  ) |> 
+    padTanzania = map2( .x = tbemTanzania, .y = tbemWorld, 
+                        .f = ~((.x*.y))) # TEM TANZANIA * POPULAÇÃO MUNDIAL 
   
-  select(idadeSimples,tbemWorld,Brasil,Tanzania) |> 
+  ) |>  unnest( c(padBrasil, padTanzania ) ) |> 
+  
+  filter(  !idadeSimples %in% "Total"  ) |>
+  select(idadeSimples, numPopulaWORLD, tbemWorld, padBrasil, padTanzania)   |> 
+
   pivot_longer(
-    cols = -c(idadeSimples),
-    names_to = "Pais", values_to = "nMxPad" ) |> 
-  
-  # mutate(
-  #   Pais = if_else(Pais == "Brazuca", "Brasil", Pais)) |> 
+  # cols = -c(idadeSimples,numPopulaWORLD,tbemWorld),
+  cols = -c(idadeSimples,numPopulaWORLD),
+  names_to = "Pais", values_to = "nMxPad" ) |> 
+
+# GRAFICO PADRONIZAÇÃO DIRETA 
+
   mutate(idadeSimples = if_else(idadeSimples == "80-100", "80+", idadeSimples)) |> 
   mutate(
-    idadeSimples = 
+    idadeSimples =
       factor(idadeSimples,
              levels = c("0-1","1-4","5-9","10-14","15-19","20-24",
                         "25-29","30-34", "35-39","40-44","45-49",
                         "50-54","55-59","60-64","65-69","70-74",
-                        "75-79","80+"))) |> 
-  arrange(idadeSimples) 
+                        "75-79","80+"))) |>
+  arrange(idadeSimples)
 
 dadoCalculado
 
-# GRAFICO PADRONIZAÇÃO DIRETA 
+
+
+
+
+
+
 
 ggplot(dadoCalculado,
        aes(x = idadeSimples, y = nMxPad, colour = Pais,
            group = Pais)
 ) +
   
-  geom_line(size = 1) +
+  geom_line(size = 2.5) +
   scale_y_log10() +
   
-  # scale_color_manual(values = c("lightgreen", "orange","skyblue"))+
+  scale_color_manual(labels = c("Brasil", "República Unida da Tanzânia", "População Mundial"),values = c("lightgreen", "orange","skyblue"))+
   labs(
     # title = "Taxa Especifica de Mortalidade de Homens e Mulheres em 2010 2019 e 2021",
     x = "Grupos Etários",
-    y = "Taxa de Mortalidade Padronizada \n (scala logaritmica)"
+    y = "Taxa de Mortalidade Padronizada \n (escala logarítmica)"
   ) + 
   # theme_minimal()+
   bbplot::bbc_style() +
   
   
   theme(
-    axis.title.y = element_text(size = 10, face = "plain"),
+    axis.title.y = element_text(size = 14, face = "plain"),
     axis.title.x = element_text(size = 12, face = "plain"),
     axis.ticks = element_line(color = "gray"),
-    axis.text.x = element_text(size = 8),
+    axis.text.x = element_text(size = 10),
+    axis.text.y = element_text(size = 8),
     
     panel.grid.minor = element_blank(),
     panel.grid.major.x = element_blank(),
@@ -117,15 +120,6 @@ ggplot(dadoCalculado,
 
 # COMPARAÇÃO DIRETA ----
 
-# Rodar o codigo até linha 166.
-
-
-
-dadoCompleto |>
-  filter(  idadeSimples %in% "Total"  ) |> 
-  mutate(  TBM = map2(.x = numObitos, .y = numPopulacao,
-                      .f = ~round((.x/.y)*1000, digits = 2))  ) |>  
-  unnest(TBM) 
 
 # write.csv(file = "dadosTratados/TaxasPadrDireta.csv",row.names = FALSE)
 
--- Atividades/PadronTaxas/codeTrab/Answer.PadIndireta.R
+++ Atividades/PadronTaxas/codeTrab/Answer.PadIndireta.R
@@ -1,5 +1,70 @@
 # Padronização Indireta ----
 
 
+# cálculo da TBM e TEM e PADRONIZAÇÃO----
 
+# tbemDado<-
+  dadoCompleto |> 
+  mutate(
+    # TBM_TEM = map2( .x = numObitos, .y = numPopulacao, .f = ~((.x/.y)*1000) ) # TBM
+    TBM_TEM = map2( .x = numObitos, .y = numPopulacao, .f = ~((.x/.y)) ) # TEM
+  ) |> 
+  unnest(TBM_TEM) |> 
+  pivot_wider( names_from = regiao, values_from = numObitos:TBM_TEM) |> 
+  select(
+    "idadeSimples","numPopulacao_Brzil","numObitos_WORLD","TBM_TEM_Brazil","TBM_TEM_United Republic of Tanzania"
+  ) |> 
+  mutate(
+    idadeSimples =  factor(idadeSimples,
+                           levels = c("0-1","1-4","5-9","10-14","15-19","20-24",
+                                      "25-29","30-34", "35-39","40-44","45-49",
+                                      "50-54","55-59","60-64","65-69","70-74",
+                                      "75-79","80-100","Total"
+                           )
+    )
+  ) |> 
+  arrange(idadeSimples)
+
+
+# ANSWER ------
+
+# compare as suas TBM. Descreva os cálculos e as 
+# hipóteses utilizadas nesta comparação. (Utilize os dois métodos de 
+# comparação de TBM - padronização direta e indireta)
+
+
+
+# cálculo da TBM e TEM e PADRONIZAÇÃO----
+
+
+  pivot_wider( names_from = regiao, values_from = numObitos:TBM_TEM)  |> 
+  select(
+    "idadeSimples","numPopulacao_Brazil","numPopulacao_United Republic of Tanzania","TBM_TEM_WORLD") |> 
+  mutate(
+    idadeSimples =  factor(idadeSimples,
+                           levels = c("0-1","1-4","5-9","10-14","15-19","20-24",
+                                      "25-29","30-34", "35-39","40-44","45-49",
+                                      "50-54","55-59","60-64","65-69","70-74",
+                                      "75-79","80-100","Total"
+                           )
+    )
+  ) |> 
+  arrange(idadeSimples) |> 
+  rename(
+    'popBrasil' = 'numPopulacao_Brazil',
+    'popTanzania' = 'numPopulacao_United Republic of Tanzania',
+    'temMundial' = 'TBM_TEM_WORLD'
+    )
+
+# CALCULO DA PRADRON INDIRETA ----
+
+tbemDado |> 
+  mutate(
+    padBrasil = map2( .x = popBrasil, .y = temMundial, 
+                      .f = ~((.x*.y))), # TEM BRAZIL * POPULAÇÃO MUNDIAL
+    
+    padTanzania = map2( .x = popTanzania, .y = temMundial, 
+                        .f = ~((.x*.y))) # TEM TANZANIA * POPULAÇÃO MUNDIAL 
+    
+  ) |>  unnest( c(padBrasil, padTanzania ) )
 
