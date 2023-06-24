98a19287794a41592c15f1dc4e5cb166a95f7e3e
czargab18 <cesargabrielcode@gmail.com>
2023-06-24 11:09:52 -0300

    [T2] - Atualização das questões do trabalho 2

--- Trabalho 2/code.Need.Project/q1/q1.R
+++ Trabalho 2/code.Need.Project/q1/q1.R
@@ -7,28 +7,33 @@
 
 
 
-
-
+# a) Construa as pirâmides etárias por grupos de idade para a população da UF 
+#    escolhida em 1991, 2000, 2010, 2015, 2020 e 2030 (obtenha os dados no portal 
+#    do Datasus para os anos censitários e projeções para os demais anos).
+#    Comente os resultados à luz da discussão sobre transição demográfica.
 
 
 # answer.Q1A ---
 
-# a) Construa as pirâmides etárias por grupos de idade para a população da UF
-#   escolhida em 1991, 2000, 2010, 2015, 2020 e 2030 (obtenha os dados no portal
-#   do Datasus para os anos censitários e projeções para os demais anos).
-#   Comente os resultados à luz da discussão sobre transição demográfica.
-
-# b) Para todos os anos acima mencionados, calcule os indicadores de estrutura
+# b) Para todos os anos acima mencionados, calcule os indicadores de estrutura 
 #    por idade (proporção de idosos (60 anos e mais), proporção de crianças
-#    (0 a 4 anos), proporção de jovens (0 a 14 anos), razão de dependência e
+#    (0 a 4 anos), proporção de jovens (0 a 14 anos), razão de dependência e 
 #    índice de envelhecimento). Calcule a idade média e a idade mediana.
-#    Calcule e grafique a razão de sexo por grupos de idade para 2000, 2010
-#    e 2030. Comente os resultados.
+#    Calcule e grafique a razão de sexo por grupos de idade para 2000, 2010 e
+#    2030. Comente os resultados.
+
+
 
 # c) Avalie a qualidade da declaração de idade no Censo 2010 segundo forma de
 #    declaração (data de nascimento e idade presumida). Calcule os índices de
 #    Whipple, Myers e Bachi. Construa a pirâmide por idade simples. Comente os
-#    resultados. (utilize a planilha SINGAGE do PAS - disponível na plataforma)
-#    Obtenha os dados na Tabela 3.2.1.2 do Censo 2010 - Resultados do Universo
-#    (https://www.ibge.gov.br/estatisticas/sociais/populacao/9662-censo-demografico-2010.html?edicao=10503&t=downloads)
+    # resultados. (utilize a planilha SINGAGE do PAS - disponível na plataforma)
 
+# Obtenha os dados na Tabela 3.2.1.2 do Censo 2010 - Resultados do Universo 
+# (https://www.ibge.gov.br/estatisticas/sociais/populacao/9662-censo-demografico-2010.html?edicao=10503&t=downloads)
+
+
+
+
+
+# .final
---
+++ Trabalho 2/code.Need.Project/q1/q2.R
@@ -0,0 +1,39 @@
+
+
+# Parte 2 - Projeção de População
+
+# a) Com base na publicação 'Projeções da população : Brasil e
+#    unidades da federação : revisão 2018" disponível no link:
+#    https://biblioteca.ibge.gov.br/visualizacao/livros/liv101597.pdf
+
+#    e na publicação "ESTIMATIVAS DA POPULAÇÃO RESIDENTE PARA OS MUNICÍPIOS E
+#    PARA AS UNIDADES DA FEDERAÇÃO COM DATA DE REFERÊNCIA EM 1º DE JULHO DE 2021 " 
+#    disponível no link:  https://biblioteca.ibge.gov.br/visualizacao/livros/liv101849.pdf
+
+# comente sobre:
+
+# I - Metodologia utilizada para projetar a população do país segundo UF.
+# II - Metodologia para estimar populações dos municípios brasileiros
+
+# b) Assumindo população fechada (ausência de migração), projete a população da 
+#   UF escolhida de 2010 a 2020, segundo o cenário de mortalidade e fecundidade 
+#   constante . Obtenha indicadores de fecundidade e mortalidade para o período
+#   2014-2016 (nascimentos e óbitos médios do triênio e população projetada pelo
+#   IBGE para 2015) . Construa uma tábua de vida e obtenha as taxas específicas 
+#   de fecundidade.
+
+# c) No link https://www.ibge.gov.br/estatisticas/sociais/populacao/9109-projecao-da-populacao.html?=&t=downloads
+#    estão disponíveis as projeções da população por sexo e idade para o Brasil 
+#    (2010-2060) e para as Unidades da Federação - revisão 2018, realizadas pelo IBGE.
+
+#   Com base nessas informações, construa e compare as pirâmides resultantes para 
+#   os anos de 2010, 2020 e 2060 para o Brasil.
+  
+#   Compare os resultados da projeção para a UF escolhida realizada no item (b)
+#   e a publicada pelo IBGE
+
+
+# * Para a análise dos resultados utilize indicadores de estrutura para
+#   respectivos anos (como os calculados na Parte 2 deste trabalho). Também
+#   comente  sobre os indicadores da dinâmica - taxa bruta de mortalidade, de
+#   natalidade e taxa de crescimento.
