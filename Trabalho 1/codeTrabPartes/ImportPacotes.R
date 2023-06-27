9f4fa1f7cb9785a183297f9e6a90f9025720b4d1
czargab18 <cesargabrielcode@gmail.com>
2023-06-27 14:19:06 -0300

    [T1] - Correções código

--- Trabalho 1/codeTrabPartes/ImportPacotes.R
+++ Trabalho 1/codeTrabPartes/ImportPacotes.R
@@ -45,11 +45,41 @@ dados |>
   imap(~ write.csv(.x, file = paste0("caminho/para/pasta/", sistemas[.y], ".csv"), row.names = FALSE))
 
 
+# correção dos dados
+
+simdf$dtnasc[
+  simdf$dtnasc %in% c("00001916", "10001950", "0001914", "00001922", "'11120001",
+                      "0151 927", "23061004")
+] <- c("03111916", "15061950", "22071914","16121922", "11041943","31031927",
+       "13061905")
+
+
+# BASE DE DADOS 
+
+sinascdf<-
+  readr::read_csv(file = "Trabalho 1/dataProject/SINASC.csv") |> 
+  janitor::clean_names() |> 
+  mutate(  dtnasc = lubridate::dmy(dtnasc),
+           ano = format(lubridate::dmy(dtnasc), "%Y")  )
+
+simdf<-
+  readr::read_csv(file = "Trabalho 1/dataProject/SIM-DO.csv") |> 
+  janitor::clean_names() |> 
+  mutate(
+    dtobito = lubridate::dmy(dtobito),
+    dtnasc = lubridate::dmy(
+      iconv(dtnasc, from = "UTF-8", to = "UTF-8" )   ),
+    ano = format(lubridate::dmy(dtobito), "%Y") 
+  )
+
+
 # População Projetada -------------------------------------------------------------------
 
 
 
 # "PROJEÇÕES POPULAÇÃO"
+
+library(readxl)
 
 url <- "https://ftp.ibge.gov.br/Projecao_da_Populacao/Projecao_da_Populacao_2018/projecoes_2018_populacao_2010_2060_20200406.xls"
 
@@ -113,7 +143,7 @@ popHom <-
 
 
 
-# TabuaVida ---------------------------------------------------------------
+# Tabua Vida ---------------------------------------------------------------
 
 
 url <- "https://unbbr-my.sharepoint.com/:x:/r/personal/211029058_aluno_unb_br/_layouts/15/Doc.aspx?sourcedoc=%7B3E1B5995-A3DE-45C5-842C-1B3AD4D2914B%7D&file=Tabuas_Mortalidade%202010-2060.xls&action=default&mobileredirect=true&DefaultItemOpen=1&login_hint=211029058%40aluno.unb.br&ct=1687812093347&wdOrigin=OFFICECOM-WEB.START.EDGEWORTH&cid=775c3831-e274-4fc2-9566-e0dc730c594d&wdPreviousSessionSrc=HarmonyWeb&wdPreviousSession=97bd440d-908a-4850-9121-4b8a5680f6aa"
@@ -188,65 +218,6 @@ TabVidaMasculina<-
 
 
 # .final
-
-
-
-#  code desnecessário  ------
-
-# Dados SINASC e SIM -------------------------------------------------------------------
-
-
-
-
-'IMPORTANDO SINASC'
-pf.sinasc<-fs::dir_ls("data.Need.Project/sinasc-dados/", glob = "*.DBC|*.dbc") |>
-  stringr::str_remove_all(pattern = ".DBC|.dbc") |>
-  stringr::str_c(pattern=".dbc")
-
-sinascdf<-
-  purrr::map2(
-    .x = pf.sinasc,
-    .y = 2000:2021,
-    .f = ~ read.dbc::read.dbc(.x) |>
-      # cria coluna ANO dentro de cada lista
-      dplyr::mutate(ano = .y),
-    .progress = TRUE
-  ) |>
-  dplyr::bind_rows() |>
-  janitor::clean_names()
-
-rm(pf.sinasc)
-
-'IMPORTANDO SIM'
-
-pf.sim<-fs::dir_ls("data.Need.Project/sim-dados/", glob = "*DOGO*.DBC|*DOGO*.dbc") |>
-  stringr::str_remove_all(pattern = ".DBC|.dbc") |>
-  stringr::str_c(pattern=".dbc")
-
-simdf<-
-  purrr::map2(
-    .x = pf.sim,
-    .y = 2000:2021,
-    .f = ~ read.dbc::read.dbc(.x) |> dplyr::mutate(ano = .y),
-    .progress = TRUE
-  ) |>
-  dplyr::bind_rows() |>
-  janitor::clean_names()
-
-rm(pf.sim)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- Trabalho 1/codeTrabPartes/q1.R
+++ Trabalho 1/codeTrabPartes/q1.R
@@ -1,5 +1,29 @@
 
 # code.Need.Analise.data ------
+
+
+sinascdf<-
+  readr::read_csv(file = "Trabalho 1/dataProject/SINASC.csv") |> 
+  janitor::clean_names() |> 
+  mutate(
+    dtnasc = lubridate::dmy(dtnasc),
+    ano = format(dtnasc, "%Y"),
+  )
+
+simdf<-
+  readr::read_csv(file = "Trabalho 1/dataProject/SIM-DO.csv") |> 
+  janitor::clean_names() |> 
+  mutate(
+    dtobito = lubridate::dmy(dtobito),
+    dtnasc = lubridate::dmy(
+      iconv(dtnasc, from = "UTF-8", to = "UTF-8" )   ),
+    
+    ano = format(dtobito, "%Y"),
+    
+  )
+
+
+
 "como 'sim.data$idade %in% 405' cuja 'id.simples' >= 5,
 a seleção FOI feita de 000:404"
 
@@ -9,7 +33,7 @@ Observação: Há um individuo que MORREU 1 DIAA ANTES
  de completar 5 anos de vida. Logo, entre em menor <5 anos"
 
 sim.data<-
-  simdf[simdf$idade %in% 000:404,c("idade","ano","dtobito", "dtnasc")] |>
+  simdf[simdf$idade %in% 000:404,c("idade","dtobito", "dtnasc")] |>
   as.data.frame() |>
   rename(data.obito=dtobito,data.nasc=dtnasc) |>
   mutate(
@@ -29,9 +53,7 @@ rm(sim.data)
 
 # Numero de NASCIMENTOS DO SINASC
 nascimTotal<-
-  sinascdf$dtnasc |>
-  stringr::str_replace(" ","") |>
-  lubridate::dmy() |>
+  sinascdf$dtnasc |> 
   format("%Y") |>
   table() |>
   data.frame() |>
@@ -40,29 +62,47 @@ nascimTotal<-
 
 # Numero de óbtos PELO DO SIM
 mortosTotal<-
-  simdf |> 
-  select(ano, dtobito, idade)|> 
+  simdf |>   select(dtobito, idade)|> 
   mutate(
-    dtobito = format(lubridate::dmy(dtobito), "%Y")
-  ) |>
-  filter( ano %in% 2000:2021 & idade %in% 000:404
-  ) |> 
-  select(ano)|> 
-  table() |>
-  data.frame() |>
-  rename(obtos=Freq)
+    dtobito = format(dtobito,"%Y")) |> 
+  filter( dtobito %in% 2000:2021 & idade %in% 000:404   ) |> 
+  select(dtobito)|>  table() |>  data.frame() |>
+
+  rename(  nobtos = Freq,   ano = dtobito )
+
+
+
+obtosIdadeAno<-
+  simdf |>
+    select(dtobito, idade)|>
+    mutate(
+      dtobito = format(dtobito,"%Y")) |>
+    filter(dtobito %in% 2000:2021)  |>
+    group_by(dtobito, idade) |> as.data.frame() |>
+    arrange(dtobito, idade) |>
+    mutate(idade =
+             ifelse(as.numeric(idade) <= 400,   0,   idade) ) |>
+    table() |> as.data.frame() |>
+  rename(  ano = dtobito) |>
+  filter(  idade %in%  c(0,401:404)   )  |>
+  mutate(  idade =   stringr::str_remove_all(idade,pattern = "40")  ) 
+
+
+obtosIdadeAno$label <- paste("Freq:", obtosIdadeAno$Freq)
+
 
 
 # JUNTANDO BASES
-dadoDiagLex <-
-  merge(
-    x = nascimTotal,
-    y = mortosTotal,
-    by = "ano") |> 
-  filter( ano ==  2000:2021
-  )
+# dadoDiagLex <-
+#   merge(
+#     x = nascimTotal,
+#     y = mortosTotal,
+#     by = "ano") |> 
+#   # merge(y = obtosIdadeAno, by = "ano") |> 
+#   filter( ano ==  2000:2021
+#   )
 
-write.csv(x = dadoDiagLex, file = "dadoTratado/q1/dadoDiagLex.csv",  row.names = FALSE)
+# write.csv(x = dadoDiagLex, file = "dadoTratado/q1/dadoDiagLex.csv",  row.names = FALSE)
 
 
 # code.Respost.Need.Q1A -----------------------------------------------------------------------
@@ -71,26 +111,28 @@ write.csv(x = dadoDiagLex, file = "dadoTratado/q1/dadoDiagLex.csv",  row.names =
 library(LexisPlotR)
 library(ggplot2)
 
-diagLexis <-
-LexisPlotR::lexis_grid(
-  year_start = 2000,
-  year_end = 2021,
-  age_start = 0,
-  age_end = 5,
-  delta = 1
-) +
-  
-  # DIAGRAMA - LEXIS  FINAL 
-  annotate("text",
-           x = seq(as.Date("2000-7-1"), as.Date("2021-7-1"), "years"),
-           y = 0.13,
-           label = dadoDiagLex$nascidos,
-           color='#1b429e',size = 3) +
-  annotate("text",
-           x = seq(as.Date("2000-8-1"), as.Date("2021-8-1"), "years"),
-           y = 2.5,
-           label = dadoDiagLex$obtos,
-           color='red',size = 3.7) +
+# diagLexis <-
+  LexisPlotR::lexis_grid( 
+    year_start = 2000, 
+    year_end = 2021, 
+    age_start = 0, 
+    age_end = 5, 
+    delta = 1,
+)
+ 
+     
+  # 
+  # # DIAGRAMA - LEXIS  FINAL 
+  # annotate("text",
+  #          x = seq(as.Date("2000-7-1"), as.Date("2021-7-1"), "years"),
+  #          y = 0.13,
+  #          label = dadoDiagLex$nascidos,
+  #          color='#1b429e',size = 3) +
+  # annotate("text",
+  #          x = seq(as.Date("2000-8-1"), as.Date("2021-8-1"), "years"),
+  #          y = 2.5,
+  #          label = dadoDiagLex$obtos,
+  #          color='red',size = 3.7) 
   
   # ROTULOS DO DIAGRAMA 
   labs(
@@ -131,6 +173,7 @@ rm(nascimTotal)
 
 # code.Need.Q1B -----------------------------------------------------------------------
 library(tidyverse)
+
 # coluna 'IDADE', dados da 'DECLARAÇÃO DE OBITO'
 "4: Anos, o segundo subcampo varia de 00 a 99.
     Exemplo: 410 == 10 anos"
@@ -140,8 +183,6 @@ library(tidyverse)
 # Numero de NASCIMENTOS DO SINASC
 nascim<-
   sinascdf$dtnasc |>
-  stringr::str_replace(" ","") |>
-  lubridate::dmy() |>
   format("%Y") |>
   table() |>
   data.frame() |>
@@ -152,39 +193,32 @@ nascim<-
 # Numero de óbtos PELO DO SIM
 mortos<-
   simdf |> 
-  select(ano, dtobito, idade)|> 
+  select(dtobito, idade)|> 
   mutate(
-    dtobito = format(lubridate::dmy(dtobito), "%Y")
+    dtobito = format(dtobito, "%Y")
   ) |>
-  filter( ano %in% 2000:2016 & idade %in% 000:404
+  filter( dtobito %in% 2000:2016 & idade %in% 000:404
   ) |> 
-  select(ano)|> 
+  select(dtobito)|> 
   table() |>
   data.frame() |>
-  rename(obtos=Freq)
+  rename(obtos=Freq,  ano = dtobito)
 
 
 # JUNTANDO BASES
 dadoSobrev5 <-
-  merge(
-    x = nascim,
-    y = mortos,
-    by = "ano") |> 
+  merge( x = nascim, y = mortos, by = "ano") |> 
   mutate(
-    pb.sobrev=map2(
-      .x =obtos,
-      .y = nascidos,
+    pb.sobrev5=map2( .x =obtos, .y = nascidos,
+                    
       # sobrev = (1 - morrer), onde morrer = /mortosnascimentos
-      .f = ~round(1-((.x/.y)),digits=4),
-      .progress = TRUE
-    )
+      .f = ~round(1-((.x/.y)),digits=3)     )
   ) |>
-  unnest(pb.sobrev) 
+  unnest(pb.sobrev5) 
 
 
 write.csv(x = dadoSobrev5,
-  file = "dadoTratado/q1/dadoSobrev5.csv",  row.names = FALSE
-)
+          file = "Trabalho 1/dadoTratado/q1/dadoSobrev5.csv",  row.names = FALSE )
 
 
 
@@ -199,8 +233,6 @@ rm(dadoSobrev5)
 # Numero de NASCIMENTOS DO SINASC
 nascim<-
   sinascdf$dtnasc |>
-  stringr::str_replace(" ","") |>
-  lubridate::dmy() |>
   format("%Y") |>
   table() |>
   data.frame() |>
@@ -211,39 +243,31 @@ nascim<-
 # Numero de óbtos PELO DO SIM
 mortos<-
   simdf |> 
-  select(ano, dtobito, idade)|> 
+  select( dtobito, idade)|> 
   mutate(
-    dtobito = format(lubridate::dmy(dtobito), "%Y")
+    dtobito = format(dtobito, "%Y")
   ) |>
-  filter( ano %in% 2000:2020 & idade %in% 000:401
+  filter( dtobito %in% 2000:2020 & idade %in% 000:401
   ) |> 
-  select(ano)|> 
+  select(dtobito)|> 
   table() |>
   data.frame() |>
-  rename(obtos=Freq)
+  rename(obtos=Freq, ano =dtobito)
 
 
 # JUNTANDO BASES
 dadoSobrev1 <-
-  merge(
-    x = nascim,
-    y = mortos,
-    by = "ano") |> 
+  merge(  x = nascim,  y = mortos,  by = "ano") |> 
   mutate(
-    pb.sobrev=map2(
-      .x =obtos,
-      .y = nascidos,
+    pb.sobrev1=map2(  .x =obtos,  .y = nascidos,
       
       # sobrev = (1 - morrer), onde morrer = /mortosnascimentos
-      .f = ~round(1-((.x/.y)),digits=4),
-      .progress = TRUE
-    )
-  ) |>
-  unnest(pb.sobrev) 
+      .f = ~round(1-((.x/.y)),digits=3)   )   ) |>
+  unnest(pb.sobrev1) 
 
 
 write.csv(x = dadoSobrev1,
-          file = "dadoTratado/q1/dadoSobrev1.csv",  row.names = FALSE
+          file = "Trabalho 1/dadoTratado/q1/dadoSobrev1.csv",  row.names = FALSE
 )
 
 
