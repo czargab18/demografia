9c5510b14dfcd7408cd1874bf30c7b7294b73da2
czargab18 <cesargabrielcode@gmail.com>
2023-07-02 22:59:31 -0300

    [T2] - Correções - dados e Piramide

--- Trabalho 2/code.Need.Project/ImportData.R
+++ Trabalho 2/code.Need.Project/ImportData.R
@@ -1,5 +1,5 @@
  # q1 -----
- library(tidyverse)
+library(tidyverse)
 library(foreign) # ler .dbf
 
 # Diretórios dos arquivos
@@ -15,60 +15,51 @@ padrao1014 <- "^1[0-4]-1[0-4]"
 padrao1519 <- "^1[5-9]-1[5-9]"
 
 ordemetaria<-
-  c("0-4","5-9","10-14","15-19", "20-24", "25-29", "30-34",
+  c("0-1","1-4","5-9","10-14","15-19", "20-24", "25-29", "30-34",
     "35-39","40-44","45-49","50-54","55-59","60-64","65-69",
     "70-74","75-79","80+")
 
-rm(pop1991)
+
+
 
 # POPBR91   1991 - ESTIMATIVA ----
 pop1991<-
   foreign::read.dbf(file = 'Trabalho 2/dataProject/file/POPBR91.DBF') |> 
   janitor::clean_names() |> 
   dplyr::filter( grepl("^52", munic_res)) |> 
-  dplyr::select(-munic_res) |> 
+  dplyr::select(-munic_res) |>
     dplyr::filter( 
     str_detect(fxetaria, pattern = padrao)) |> 
   dplyr::mutate(
     fxetaria = map_chr(as.character(fxetaria), ~ if (nchar(.) == 4){ paste0(substr(., 1, 2), "-", substr(., 3, 4))} else {.})
   )|>
-  group_by(sexo, fxetaria) |> 
+  dplyr::group_by(sexo, fxetaria) |> 
   
-  mutate(fxetaria = case_when(
-    # caso 0-4 anos
-    fxetaria %in% c("00-00", "01-01", "02-02", "03-03", "04-04") ~ "0-4",
-    str_detect(fxetaria, pattern = padrao04) ~ "0-04",
+  dplyr::mutate(fxetaria = case_when(
+    fxetaria %in% c("00-00", "01-01") ~ "0-1",
     
-    # caso 5-9 anos
+    fxetaria %in% c("01-01", "02-02", "03-03", "04-04") ~ "1-4",
+    
     fxetaria %in% c("05-05","06-06", "07-07", "08-08","09-09") ~ "5-9",
-    str_detect(fxetaria, pattern = padrao59) ~ "5-9",
-    
-    # caso 10-14 anos
+ 
     fxetaria %in% c("10-10","11-11", "12-12", "13-13", "14-14") ~ "10-14",
-    str_detect(fxetaria, pattern = padrao1014) ~ "10-14",
-    
-    # caso 15-19 anos
     fxetaria %in% c("15-15","16-16", "17-17", "18-18", "19-19") ~ "15-19",
-    str_detect(fxetaria, pattern = padrao1014) ~ "15-19",
-    
-    # caso 80-99 anos
     fxetaria %in% c("80-99") ~ "80+",
-    str_detect(fxetaria, pattern = padrao1014) ~ "80+",
-    
-    
+
     TRUE ~ fxetaria ),
   #  reordenando os grupos etários
     fxetaria = factor(forcats::as_factor(fxetaria),  levels = ordemetaria)) |> 
   
   
-  group_by(sexo, fxetaria) |> 
+  dplyr::group_by(sexo, fxetaria) |> 
   dplyr::summarise(populacao = sum(populacao)) |> 
-  arrange(fxetaria)
+  dplyr::arrange(fxetaria)
 
 
 
 
-# POPBR00 2000 - ESTIMATIVA 
+
+# POPBR00 2000 - ESTIMATIVA ----
 pop2000<-
   foreign::read.dbf(file = 'Trabalho 2/dataProject/file/POPBR00.DBF') |>
   janitor::clean_names() |> 
@@ -82,29 +73,17 @@ pop2000<-
   group_by(sexo, fxetaria) |> 
   
   mutate(fxetaria = case_when(
-    # caso 0-4 anos
-    fxetaria %in% c("00-00", "01-01", "02-02", "03-03", "04-04") ~ "0-4",
-    str_detect(fxetaria, pattern = padrao04) ~ "0-04",
+    fxetaria %in% c("00-00", "01-01") ~ "0-1",
     
-    # caso 5-9 anos
+    fxetaria %in% c("01-01", "02-02", "03-03", "04-04") ~ "1-4",
+
     fxetaria %in% c("05-05","06-06", "07-07", "08-08","09-09") ~ "5-9",
-    str_detect(fxetaria, pattern = padrao59) ~ "5-9",
-    
-    # caso 10-14 anos
     fxetaria %in% c("10-10","11-11", "12-12", "13-13", "14-14") ~ "10-14",
-    str_detect(fxetaria, pattern = padrao1014) ~ "10-14",
-    
-    # caso 15-19 anos
+
     fxetaria %in% c("15-15","16-16", "17-17", "18-18", "19-19") ~ "15-19",
-    str_detect(fxetaria, pattern = padrao1014) ~ "15-19",
-    
-    # caso 80-99 anos
     fxetaria %in% c("80-99") ~ "80+",
-    str_detect(fxetaria, pattern = padrao1014) ~ "80+",
-    
-    
+
     TRUE ~ fxetaria ),
-    #  reordenando os grupos etários
     fxetaria = factor(forcats::as_factor(fxetaria),  levels = ordemetaria)) |> 
   
   
@@ -114,7 +93,8 @@ pop2000<-
 
 
 
-# POPTBR10.csv  2010
+
+# POPTBR10.csv  2010 ----
 pop2010<-
   foreign::read.dbf(file = 'Trabalho 2/dataProject/file/POPBR10.DBF') |>  
   janitor::clean_names() |> 
@@ -128,26 +108,16 @@ pop2010<-
   group_by(sexo, fxetaria) |> 
   
   mutate(fxetaria = case_when(
-    # caso 0-4 anos
-    fxetaria %in% c("00-00", "01-01", "02-02", "03-03", "04-04") ~ "0-4",
-    str_detect(fxetaria, pattern = padrao04) ~ "0-04",
+    fxetaria %in% c("00-00", "01-01") ~ "0-1",
     
-    # caso 5-9 anos
+    fxetaria %in% c("01-01", "02-02", "03-03", "04-04") ~ "1-4",
     fxetaria %in% c("05-05","06-06", "07-07", "08-08","09-09") ~ "5-9",
-    str_detect(fxetaria, pattern = padrao59) ~ "5-9",
     
-    # caso 10-14 anos
     fxetaria %in% c("10-10","11-11", "12-12", "13-13", "14-14") ~ "10-14",
-    str_detect(fxetaria, pattern = padrao1014) ~ "10-14",
-    
-    # caso 15-19 anos
     fxetaria %in% c("15-15","16-16", "17-17", "18-18", "19-19") ~ "15-19",
-    str_detect(fxetaria, pattern = padrao1014) ~ "15-19",
-    
-    # caso 80-99 anos
+
     fxetaria %in% c("80-99") ~ "80+",
-    str_detect(fxetaria, pattern = padrao1014) ~ "80+",
-    
+
     
     TRUE ~ fxetaria ),
     #  reordenando os grupos etários
@@ -158,11 +128,107 @@ pop2010<-
   dplyr::summarise(populacao = sum(populacao)) |> 
   arrange(fxetaria)
 
+# desnecessário.
+rm(padrao)
+rm(padrao04)
+rm(padrao59)
+rm(padrao1014)
+rm(padrao1519)
 
 
 
+# TabuaVida 2010  sinasc - sim ------------------------------------------------------------
 
 
+# ano 2010 - nascimentos
+sinasc2010<-
+ microdatasus::fetch_datasus( uf = 'GO', information_system = 'SINASC', year_start = 2010, year_end = 2010, vars = c('DTNASC','SEXO') ) |> 
+  dplyr::mutate(
+    # ano = format(lubridate::dmy(DTNASC), '%Y'),
+    DTNASC = format( lubridate::dmy(  iconv( DTNASC, from = "UTF-8", to = "UTF-8" )),  '%Y') ) |> 
+  group_by(SEXO,DTNASC) |>  count() |>    filter(  !SEXO %in% '9' ) |> 
+  rename( nascimentos = n)
+
+  
+#  SIM - 2010
+sim2010<-
+  microdatasus::fetch_datasus(
+    uf = 'GO', 
+    information_system = 'SIM-DO', 
+    year_start = 2010, 
+    year_end = 2010,
+    # vars = c('IDADE','SEXO','DTNASC','DTOBITO')
+    vars = c('IDADE','SEXO','DTOBITO')
+  ) |> 
+  dplyr::mutate(
+    SEXO = as.numeric(as.character(SEXO)),
+    DTOBITO = format( lubridate::dmy( iconv(  DTOBITO, from = "UTF-8", to = "UTF-8" )), '%Y'),
+    contador = 1,
+   
+     IDADE = case_when(
+      IDADE %in% 000:400 ~ "0-1",
+      IDADE %in% 401:404 ~ "1-4",
+      
+      IDADE %in% 405:409 ~ "5-9",
+      IDADE %in% 410:414 ~ "10-14",
+      
+      IDADE %in% 415:419 ~ "15-19",
+      IDADE %in% 420:424 ~ "20-24",
+      IDADE %in% 425:429 ~ "25-29",
+      
+      IDADE %in% 430:434 ~ "30-34",
+      IDADE %in% 435:439 ~ "35-39",
+      IDADE %in% 440:444 ~ "40-44",
+      
+      IDADE %in% 445:449 ~ "45-49",
+      IDADE %in% 450:454 ~ "50-54",
+      IDADE %in% 455:459 ~ "55-59",
+      
+      IDADE %in% 460:464 ~ "60-64",
+      IDADE %in% 465:469 ~ "65-69",
+      IDADE %in% 470:474 ~ "70-74",
+      
+      IDADE %in% 475:479 ~ "75-79",
+      IDADE %in% 480:499 ~ "80+",
+      
+      TRUE ~ IDADE ),
+    IDADE = factor(forcats::as_factor(IDADE),  levels = ordemetaria)
+    ) |> 
+  filter( !SEXO %in% '9') |>  
+  group_by(SEXO, IDADE) |> 
+  dplyr::summarise(OBTOS = sum(contador)) |> 
+  arrange(IDADE)
+
+# dadoCompleto ------------------------------------------------------------
+
+
+'dadoCompleto'
+dadoCompleto<-
+  merge(x = pop2010, y = sinasc2010,  by.x = 'sexo', by.y = 'SEXO') |> 
+    rename(SEXO = sexo, IDADE = fxetaria) |> 
+    merge(y = sim2010, by = c('SEXO', 'IDADE')) |> 
+  mutate(
+    nMx = ifelse(IDADE %in% c('0-1', '1-4'),
+                 map2(OBTOS, nascimentos, ~ round((.x / .y), digits = 6)),
+                 map2(OBTOS, populacao, ~ round((.x / .y), digits = 6))
+    ),
+    IDADE = factor(forcats::as_factor(IDADE),  levels = ordemetaria)  
+  ) |> 
+  unnest(nMx) |> 
+    arrange(SEXO,IDADE)
+
+dadoCompleto
+  
+
+
+# export data -------------------------------------------------------------
+
+
+write.csv(x = dadoCompleto,
+          file = "Trabalho 2/dadoTratado/CompletoTEspecifMeTMInfan.xls",  row.names = FALSE )
+
+write.csv(x = dadoCompleto,
+          file = "Trabalho 2/dadoTratado/CompletoTEspecifMeTMInfan.csv",  row.names = FALSE )
 
 
 # q2 ----------------------------------------------------------------------
--- Trabalho 2/code.Need.Project/q1.R
+++ Trabalho 2/code.Need.Project/q1.R
@@ -1,9 +1,14 @@
 'Aberto: terça-feira, 4 abr. 2023, 22:10'
 'Vencimento: segunda-feira, 10 jul. 2023, 23:59'
 
+library(tidyverse)
 library(ggplot2)
 options(scipen = 99999)
 
+ordemetaria<-
+  c("0-4","5-9","10-14","15-19", "20-24", "25-29", "30-34",
+    "35-39","40-44","45-49","50-54","55-59","60-64","65-69",
+    "70-74","75-79","80+")
 
 # Parte 1 - Estrutura Populacional e avaliação da informação sobre idade
 
@@ -15,6 +20,25 @@ options(scipen = 99999)
 #    Comente os resultados à luz da discussão sobre transição demográfica.
 
 'anos cesitários :  1991, 2000, 2010 ||| projetar: 2020 e 2030'
+# graficos PirEtar ----
+
+"GRAFICO 1991"
+
+pop1991<-
+  pop1991 |> group_by(sexo, fxetaria) |> 
+  mutate(
+    fxetaria = case_when(
+      fxetaria %in% '0-1' ~ "0-4",
+      fxetaria %in% '1-4' ~ "0-4",
+      TRUE~fxetaria
+    ),
+    fxetaria = factor(forcats::as_factor(fxetaria),  levels = ordemetaria)
+  ) |> 
+  filter( !fxetaria %in% '9') |>  
+  group_by(sexo, fxetaria) |> 
+  dplyr::summarise(populacao = sum(populacao)) |> 
+  arrange(fxetaria)
+
 
 ggplot(data = pop1991, mapping = aes(x = fxetaria)) +
   geom_bar(data = filter(pop1991, sexo == 2), aes(y = populacao,fill= sexo),
@@ -26,25 +50,95 @@ ggplot(data = pop1991, mapping = aes(x = fxetaria)) +
   
   # girar gráfico 
   coord_flip() +
-  theme_minimal()+
-  theme( 
-    panel.grid.major.x = element_line(size = 0.7, color = "gray"),
-    panel.grid.major.y = element_line(size = 0.5),
-    axis.text.x = element_text(size = 14),
-    axis.text.y = element_text(size = 12)
-        )+
-  
+  theme_minimal() +
   scale_color_manual(
-    values =  c('#f95d06','#343496'),aesthetics = 'fill',
+    values =  c('#343496','#f95d06'),aesthetics = 'fill',
     labels = c("Homens", "Mulheres")) +
-  
+     
+
+  theme( 
+      panel.grid.major.x = element_line(linewidth = 0.7, color = "gray"),
+      panel.grid.major.y = element_line(linewidth = 0.5),
+      axis.text.x = element_text(size = 14),
+      axis.text.y = element_text(size = 12)
+    ) +
   labs(
     x = "Grupos Etários",
-    y = "Distribuição da população \n (em centenas de pessoas)",
+    y = "Distribuição da população \n (em milhares de pessoas)",
     fill = "Sexo",
     title = "Pirâmide Etária de 1991, Goiás",
     caption = "Fonte: Datasus, 1991"
   )
+
+"GRAFICO 2000"
+
+ggplot(data = pop2000, mapping = aes(x = fxetaria)) +
+  geom_bar(data = filter(pop2000, sexo == 2), aes(y = populacao,fill= sexo),
+           stat = "identity") +
+  
+  geom_bar(data = filter(pop2000, sexo == 1), aes(y = -populacao, fill= sexo),
+           stat = "identity") +
+  scale_y_continuous(labels = abs)+  # e options(scipen = 99999)
+  # scale_y_continuous(labels = comma(populacao, scientific=F))+
+  
+  # girar gráfico 
+  coord_flip() +
+  theme_minimal()+
+  theme( 
+    panel.grid.major.x = element_line(linewidth = 0.7, color = "gray"),
+    panel.grid.major.y = element_line(linewidth = 0.5),
+    axis.text.x = element_text(size = 14),
+    axis.text.y = element_text(size = 12)
+  )+
+  
+  scale_color_manual(
+    values =  c('#343496','#f95d06'),aesthetics = 'fill',
+    labels = c("Homens", "Mulheres")) +
+  
+  labs(
+    x = "Grupos Etários",
+    y = "Distribuição da população \n (em milhares de pessoas)",
+    fill = "Sexo",
+    title = "Pirâmide Etária de 2000, Goiás",
+    caption = "Fonte: Datasus, 2000"
+  )
+
+
+"GRAFICO 2010"
+ggplot(data = pop2010, mapping = aes(x = fxetaria)) +
+  geom_bar(data = filter(pop2010, sexo == 2), aes(y = populacao,fill= sexo),
+           stat = "identity") +
+  
+  geom_bar(data = filter(pop2010, sexo == 1), aes(y = -populacao, fill= sexo),
+           stat = "identity") +
+  scale_y_continuous(labels = abs)+
+  
+  # girar gráfico 
+  coord_flip() +
+  theme_minimal()+
+  theme( 
+    panel.grid.major.x = element_line(linewidth = 0.7, color = "gray"),
+    panel.grid.major.y = element_line(linewidth = 0.5),
+    axis.text.x = element_text(size = 14),
+    axis.text.y = element_text(size = 12)
+  )+
+  
+  scale_color_manual(
+    values =  c('#343496','#f95d06'),aesthetics = 'fill',
+    labels = c("Homens", "Mulheres")) +
+  
+  labs(
+    x = "Grupos Etários",
+    y = "Distribuição da população \n (em milhares de pessoas)",
+    fill = "Sexo",
+    title = "Pirâmide Etária de 2010, Goiás",
+    caption = "Fonte: Datasus, 2010"
+  )
+
+
+# .projeções ----
+
+pop1991
 
 # answer.Q1A ---
 
