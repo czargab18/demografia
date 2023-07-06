201345eacdee8b44e1fda37b54a11d058b6aa8f0
czargab18 <cesargabrielcode@gmail.com>
2023-07-06 17:16:57 -0300

    [T2] - Organizando as Questões

---
+++ Trabalho 2/code.Need.Project/q2c.R
@@ -0,0 +1,15 @@
+# c) No link https://www.ibge.gov.br/estatisticas/sociais/populacao/9109-projecao-da-populacao.html?=&t=downloads
+#    estão disponíveis as projeções da população por sexo e idade para o Brasil 
+#    (2010-2060) e para as Unidades da Federação - revisão 2018, realizadas pelo IBGE.
+
+#   Com base nessas informações, construa e compare as pirâmides resultantes para 
+#   os anos de 2010, 2020 e 2060 para o Brasil.
+
+#   Compare os resultados da projeção para a UF escolhida realizada no item (b)
+#   e a publicada pelo IBGE
+
+
+# * Para a análise dos resultados utilize indicadores de estrutura para
+#   respectivos anos (como os calculados na Parte 2 deste trabalho). Também
+#   comente  sobre os indicadores da dinâmica - taxa bruta de mortalidade, de
+#   natalidade e taxa de crescimento.
